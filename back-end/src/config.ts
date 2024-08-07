0e50b7f4573cc3d61420dec5ee3599f03881925f
alirezaizadi199912 <alirezaizadi199912@gmail.com>
2024-08-07 10:10:34 +0000

    setup database

--- back-end/src/config.ts
+++ back-end/src/config.ts
@@ -1,5 +1,6 @@
+import mongoose from "mongoose";
 import {UserRepository} from "./repository/user.repository";
 import {UserService} from "./services/user.service";
 
-export const userRepository = new UserRepository();
+export const userRepository = new UserRepository(mongoose);
 export const userService = new UserService(userRepository);
