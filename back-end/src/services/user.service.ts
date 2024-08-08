959e503a10d02631421c97fbf8107bf46a4b960e
alirezaizadi199912 <alirezaizadi199912@gmail.com>
2024-08-08 07:21:53 +0000

    write interface

--- back-end/src/services/user.service.ts
+++ back-end/src/services/user.service.ts
@@ -1,5 +1,13 @@
 import {UserRepository} from "../repository/user.repository";
 
+export interface IUserRepository {
+    // add: (registerRequest: RegisterRequest) => Promise<User>;
+    // checkEmailExistance: (email: string) => Promise<boolean>;
+    // checkUserNameExistance: (userName: string) => Promise<boolean>;
+    // getUserByEmail: (email: string) => Promise<User|undefined>;
+    // getUserByUsername: (userName: string) => Promise<User|undefined>;
+}
+
 export class UserService {
     constructor(private userRepository: UserRepository) {}
 }
