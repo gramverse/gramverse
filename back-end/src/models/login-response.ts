7819a02f7550b8773a8c560325928625c80fb096
alirezaizadi199912 <alirezaizadi199912@gmail.com>
2024-08-09 06:29:04 +0000

    Signup

--- back-end/src/models/login-response.ts
+++ back-end/src/models/login-response.ts
@@ -1,15 +1,40 @@
 export type LoginResponse = {
-    user : User | undefined;
-    token : string | undefined;
-} 
-type User = {
-    _id: string,
+    user : User,
+    token : string,
+} ;
+
+export interface IUser extends Document {
+    _id?: string;
+    userName: string;
+    firstName: string;
+    lastName: string;
+    profilePicture: string;
+    email: string;
+    passwordHash: string;
+    isPrivate: boolean;
+    bio: string;
+}
+
+export type User = {
+    _id?: string,
     userName: string,
     firstName: string,
     lastName: string,
     profilePicture: string,
     email: string,
-    passwordHash: string
-    isPublic: boolean,
+    passwordHash: string,
+    isPrivate: boolean,
     bio: string,
-}
+};
+
+export type UserToValidate = {
+    _id?: string,
+    userName: string,
+    firstName: string,
+    lastName: string,
+    profilePicture: string,
+    email: string,
+    password: string,
+    isPrivate: boolean,
+    bio: string,
+};
--- back-end/src/routes/user.route.ts
+++ back-end/src/routes/user.route.ts
@@ -1,18 +1,44 @@
 import {userService} from "../config"
 import {Router} from "express";
 import { zodLoginRequest } from "../models/login-request";
+import {zodRegisterRequest} from "../models/register-request";
 import { HttpError } from "../errors/http-error";
 import { ErrorCode } from "../errors/error-codes";
 import { LoginResponse } from "../models/login-response";
+
 export const userRouter = Router();
 
+userRouter.post("/signup", async (req, res) => {
+    try {
+        console.log(req.body);
+        const registerRequest = zodRegisterRequest.parse(req.body);
+        const loginResponse = await userService.signup(registerRequest);
+        if (!loginResponse) {
+            throw new HttpError(401, ErrorCode.UNAUTHORIZED, "Not authorized");
+        }
+        res.status(200).send(loginResponse.user);
+    } catch(err) {
+        if (err instanceof HttpError) {
+            res.status(err.statusCode).send(err);
+            return;
+        }
+        res.status(500).send();
+    }
+});
 
 userRouter.post("/login", async (req, res) => {
-    const loginRequest = zodLoginRequest.parse(req.body);
-    const loginResponse : LoginResponse = await userService.login(loginRequest)
-    if (!loginResponse.user) {
-        throw new HttpError(401, ErrorCode.UNAUTHORIZED);
+    try{
+        const loginRequest = zodLoginRequest.parse(req.body);
+        const loginResponse = await userService.login(loginRequest);
+        if (!loginResponse) {
+            throw new HttpError(401, ErrorCode.UNAUTHORIZED, "Username or password incorrect");
+        }
+        res.header("Authorization", `Bearer ${loginResponse.token}`).status(201).send(loginResponse.user);
+    } catch(err) {
+        if (err instanceof HttpError) {
+            res.status(err.statusCode).send(err);
+            return;
+        }
+        res.status(500).send();
     }
-    res.header('Authorization', `Bearer ${loginResponse.token}`).status(201).send(loginResponse.user);
-
-})
+});
