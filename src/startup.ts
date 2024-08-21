a7de896ad8ed1476172e98fd17f5a1046e538b9d
hamid akhavan <hawmidak@gmail.com>
2024-08-21 13:24:46 +0330

    swagger config

--- src/startup.ts
+++ src/startup.ts
@@ -1,4 +1,4 @@
-import express, { ErrorRequestHandler } from "express";
+import express, { ErrorRequestHandler,Request,Response,NextFunction } from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import {ZodError} from "zod";
@@ -6,6 +6,9 @@ import {HttpError} from "./errors/http-error";
 import {userRouter} from "./routes/user.route";
 import {tokenRouter} from "./routes/reset.route";
 import {fileRouter} from "./routes/file.route";
+import swaggerUi from "swagger-ui-express";
+import { swaggerDocs } from "./main";
+import swaggerJSDoc from "swagger-jsdoc";
 
 const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
     if (err instanceof ZodError) {
@@ -18,18 +21,25 @@ const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
     res.status(500).send();
 }
 
+
+const middleware = (req:Request,res:Response,next:NextFunction) => {
+    console.log(1)
+    next()
+}
+
 export const buildApp = () => {
     const app = express();
-
+    
     app.use(cors());
     app.use(cookieParser());
-    app.use("/files", fileRouter);
+    app.use("api/files", fileRouter);
     app.use(express.json());
-
+    
     // app.use(errorHandler);
     
-    app.use("/api/users", userRouter);
+    app.use("/api/users", middleware,userRouter);
     app.use("/api/reset", tokenRouter);
+    app.use("/api/api-docs",swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 
     app.use((req, res, next) => {
         res.status(404).send({messge: "Not found"});
