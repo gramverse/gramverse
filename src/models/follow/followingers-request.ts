f064107dd266b9a481ae0b1b5ad05b1c257e55bd
alirezaizadi199912 <alirezaizadi199912@gmail.com>
2024-08-29 18:43:42 +0000

    Fix query bug

--- src/models/follow/followingers-request.ts
+++ src/models/follow/followingers-request.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const zodFollowingersRequest = z.object({
     userName: z.string().nonempty(),
-    isFollowing: z.coerce.boolean(),
+    isFollowing: z.string(),
     page: z.coerce.number().min(1).default(1),   
     limit: z.coerce.number().min(1).max(100).default(10) 
 })
