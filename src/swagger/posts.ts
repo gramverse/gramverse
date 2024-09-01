19fdfc2aa19eda9373e906794ee0d1b52301bb69
alirezaizadi199912 <alirezaizadi199912@gmail.com>
2024-09-01 07:42:21 +0000

    Feature explore posts

--- src/swagger/posts.ts
+++ src/swagger/posts.ts
@@ -2,7 +2,7 @@
  * @swagger
  * /api/posts/myPosts:
  *   get:
- *     summary: Get posts for the currently logged-in user.
+ *     summary: Get created posts by the currently logged-in user.
  *     tags:
  *       - posts
  *     parameters:
@@ -235,3 +235,33 @@
  *       500:
  *         description: Internal server error.
  */
+
+/**
+ * @swagger
+ * /api/posts/explorePosts:
+ *   get:
+ *     summary: Get explore posts for the currently logged-in user.
+ *     tags:
+ *       - posts
+ *     parameters:
+ *       - in: query
+ *         name: page
+ *         schema:
+ *           type: integer
+ *         required: true
+ *         description: page number (starting from 1)
+ *       - in: query
+ *         name: limit
+ *         schema:
+ *           type: integer
+ *         required: true
+ *         description: Number of results in each page
+ *     responses:
+ *       200:
+ *         description: Posts retrieved successfully.
+ *       401:
+ *         description: Not authorized.
+ *       500:
+ *         description: Internal server error.
+ */
+
