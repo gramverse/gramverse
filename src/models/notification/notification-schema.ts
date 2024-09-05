decfd6c7889ef3935548da9b05790b8b0751d89b
hamid akhavan <hawmidak@gmail.com>
2024-09-05 18:43:38 +0330

    seen

--- src/models/notification/notification-schema.ts
+++ src/models/notification/notification-schema.ts
@@ -4,7 +4,7 @@ export const notificationSchema = new mongoose.Schema({
     userName: {type: String, required: true},
     eventId: {type: String, required: true},
     isMine: {type: Boolean, required: true},
-    seen: {type: Boolean, required: true},
+    seen: {type: Boolean, required: true,default: false},
 }, {
     timestamps: { createdAt: "creationDate", updatedAt: "updateDate" }
 })
