63cfc31c99aa740a9499a2b8c5ae97ba61a7c2a7
hamid akhavan <hawmidak@gmail.com>
2024-09-13 17:27:24 +0330

    add spec search swagger

--- src/swagger/search.ts
+++ src/swagger/search.ts
@@ -31,6 +31,37 @@
 
 /**
  * @swagger
+ * /api/search/specTag:
+ *   get:
+ *     summary: search tags and posts
+ *     tags:
+ *       - search
+ *     parameters:
+ *       - in: query
+ *         name: page
+ *         schema:
+ *           type: integer
+ *           format: int32
+ *       - in: query
+ *         name: limit
+ *         schema:
+ *           type: integer
+ *           format: int32
+ *       - in: query
+ *         name: tag
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Notifications retrieved successfully.
+ *       401:
+ *         description: Not authorized.
+ *       500:
+ *         description: Internal server error.
+ */
+
+/**
+ * @swagger
  * /api/search/Suggesttag:
  *   get:
  *     summary: search tags and posts
