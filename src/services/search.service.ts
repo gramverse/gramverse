c67648c1f679698f95f728ea05c426656b31d89e
hamid akhavan <hawmidak@gmail.com>
2024-09-13 15:52:17 +0330

    edit tags and posts name in tag search

--- src/services/search.service.ts
+++ src/services/search.service.ts
@@ -8,14 +8,14 @@ export class SearchService {
     searchTags = async (tag: string,limit: number, page: number ) => {
         const skip = (page - 1) * limit
         const totalCount = await this.tagRepository.tagCount(tag)
-        const tags = await this.tagRepository.searchTag(tag,skip,limit)
-        return{tags,totalCount}
+        const posts = await this.tagRepository.searchTag(tag,skip,limit)
+        return{posts,totalCount}
     }
     SuggestTags = async (tag: string,limit: number, page: number) => {
         const skip = (page - 1) * limit
         const totalCount = await this.tagRepository.countSuggestedTags(tag)
-        const posts = await this.tagRepository.suggestTag(tag,skip,limit)
-        return {posts, totalCount}
+        const tags = await this.tagRepository.suggestTag(tag,skip,limit)
+        return {tags, totalCount}
     }
 
 }    
