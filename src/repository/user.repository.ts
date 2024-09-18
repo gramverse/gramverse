509c1450fd01b8c7d882734d277e97e5528c8b88
alimhp2 <alimhp2@gmail.com>
2024-09-18 17:03:06 +0330

    this  commit will fix the duplication buf for account search

--- src/repository/user.repository.ts
+++ src/repository/user.repository.ts
@@ -88,6 +88,18 @@ export class UserRepository {
                 },
             },
             {
+                $project: {
+                    _id: 0, // Prevents _id from being used to differentiate duplicates
+                    accountId: "$_id",
+                    userName: 1,
+                    firstName: 1,
+                    lastName: 1,
+                    fullName: {$concat: ["$firstName", " ", "$lastName"]},
+                    profileImage: 1,
+                    followerCount: 1,
+                },
+            },
+            {
                 $group: {
                     _id: "$_id",
                     userName: {$first: "$userName"},
